52938f02358139f54665548fd8f4c8bdf990c8ff
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-27 13:13:53 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/tw-elements/dist/js/**/*.js"],
+	theme: {
+		extend: {},
+	},
+	darkMode: "class",
+	plugins: [require("tw-elements/dist/plugin.cjs")],
+};
